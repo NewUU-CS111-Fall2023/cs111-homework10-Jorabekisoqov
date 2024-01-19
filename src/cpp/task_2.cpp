df30cdc880d0fba5112fa7be3f36ef571f757eb0
Jo'rabek <116886093+isoqovjorabek2@users.noreply.github.com>
2024-01-19 20:09:52 +0500

    commited

--- src/cpp/task_2.cpp
+++ src/cpp/task_2.cpp
@@ -7,6 +7,73 @@
 class Problem2 {
 public:
 
+    enum State {
+        START,
+        FIND_SEPARATOR,
+        INCREMENT,
+        MOVE_TO_NEXT,
+        HALT
+    };
+
+// Function to simulate the Turing machine
+    void turingMachineAddition(string tape) {
+        State currentState = START;
+        int headPosition = 0;
+
+        while (currentState != HALT) {
+            char currentSymbol = tape[headPosition];
+
+            switch (currentState) {
+                case START:
+                    if (currentSymbol == '1') {
+                        currentState = FIND_SEPARATOR;
+                    } else {
+                        cout << "Invalid input format" << endl;
+                        currentState = HALT;
+                    }
+                    break;
+
+                case FIND_SEPARATOR:
+                    if (currentSymbol == '1') {
+                        tape[headPosition] = '0';  // Decrement the first number
+                        headPosition--;
+                    } else if (currentSymbol == '0') {
+                        currentState = INCREMENT;
+                        headPosition++;
+                    } else {
+                        cout << "Invalid input format" << endl;
+                        currentState = HALT;
+                    }
+                    break;
+
+                case INCREMENT:
+                    if (currentSymbol == '1') {
+                        tape[headPosition] = '0';  // Move to the next digit
+                        headPosition++;
+                    } else if (currentSymbol == '0') {
+                        currentState = MOVE_TO_NEXT;
+                    } else {
+                        cout << "Invalid input format" << endl;
+                        currentState = HALT;
+                    }
+                    break;
+
+                case MOVE_TO_NEXT:
+                    if (currentSymbol == '0') {
+                        headPosition++;
+                    } else if (currentSymbol == '1') {
+                        tape[headPosition] = '1';  // Increment the second number
+                        currentState = HALT;
+                    } else {
+                        cout << "Invalid input format" << endl;
+                        currentState = HALT;
+                    }
+                    break;
+            }
+        }
+
+        cout << "Result: " << tape << endl;
+    }
 
 
 };
